2860c707ea7b97301351fdd3d54732d6f3946472
justjavac <justjavac@gmail.com>
2020-08-21 20:55:20 +0800

    fix fmt

--- mod_test.ts
+++ mod_test.ts
@@ -5,14 +5,19 @@ import indent from "./mod.ts";
 
 Deno.test("test indent", function () {
   assertEquals(
-    indent("Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit,\nsed do eiusmod tempor incididunt."),
+    indent(
+      "Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit,\nsed do eiusmod tempor incididunt.",
+    ),
     " Lorem ipsum dolor sit amet,\n consectetur adipisicing elit,\n sed do eiusmod tempor incididunt.",
   );
 });
 
 Deno.test("test indent with empty line", function () {
   assertEquals(
-    indent("Lorem ipsum dolor sit amet,\n\n\nconsectetur adipisicing elit,\nsed do eiusmod tempor incididunt.", 1),
+    indent(
+      "Lorem ipsum dolor sit amet,\n\n\nconsectetur adipisicing elit,\nsed do eiusmod tempor incididunt.",
+      1,
+    ),
     " Lorem ipsum dolor sit amet,\n\n\n consectetur adipisicing elit,\n sed do eiusmod tempor incididunt.",
   );
 });
